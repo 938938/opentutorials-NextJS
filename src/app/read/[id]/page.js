9a75ee5c6fb38c57c8534b2858af58869dcc0105
938938 <938kjh@gmail.com>
2023-08-30 23:57:57 +0900

    [11] 읽기 기능 구현

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,8 +1,9 @@
-export default function Read(props) {
-  return (
-    <>
-      <h2>Read</h2>
-      parameter : {props.params.id}
-    </>
-  );
+export default async function Read(props){
+  const id = props.params.id;
+  const resp = await fetch(`http://localhost:9999/topics/${id}`);
+  const topic = await resp.json();
+  return <>
+    <h2>{topic.title}</h2>
+    {topic.body}
+  </>
 }

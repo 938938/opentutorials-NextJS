8ca77bea2179361a3cb544fc44d5d4667c7f9a92
938938 <938kjh@gmail.com>
2023-09-01 00:14:41 +0900

    [14] 수정 기능 구현

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,9 +1,13 @@
-export default async function Read(props){
+export default async function Read(props) {
   const id = props.params.id;
-  const resp = await fetch(`http://localhost:9999/topics/${id}`);
+  const resp = await fetch(`http://localhost:9999/topics/${id}`, {
+    cache: 'no-cache',
+  });
   const topic = await resp.json();
-  return <>
-    <h2>{topic.title}</h2>
-    {topic.body}
-  </>
+  return (
+    <>
+      <h2>{topic.title}</h2>
+      {topic.body}
+    </>
+  );
 }
